c8026f12f838c875832b3b15fb3457655be05458
JaviCeRodriguez <javicerodriguez@gmail.com>
2024-09-30 10:37:06 -0300

    add: link to posts in home

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+
+      <Link to="/posts" unstable_viewTransition>
+        Posts
+      </Link>
     </div>
   );
 };
